887ae4de8dcd83483f5efadac7cfccb82b97d16d
d032408 <bernd.kuespert@sap.com>
2023-11-17 11:56:35 +0100

    bigger update to the restart

--- restart.py
+++ restart.py
@@ -12,9 +12,11 @@ warnings.filterwarnings("ignore")
 
 def get_args():
     parser = argparse.ArgumentParser(description='Python script using Redfish API to reset(reboot) iDRAC')
-    parser.add_argument('-ip', "--ip_address", help='IP address', required=False)
-    parser.add_argument('-u', "--username", help='username', required=False)
-    parser.add_argument('-p', "--password", help='password', required=False)
+    parser.add_argument('-t', "--target", help='IP address or hostname of the remote board.', required=True)
+    parser.add_argument('-u', "--username", help='username for accessing the remote board', required=True)
+    parser.add_argument('-p', "--password", help='password for accessing the remote board', required=True)
+    parser.add_argument('-f', "--force", help = "force restart", action='store_true', required=False)
+    parser.add_argument('-to', "--timeout", help = "timeout for redfish request", default=10, required=False)
 
     if len(sys.argv)==1:
         parser.print_help()
@@ -47,49 +49,74 @@ def get_manufacturer(netbox_device):
     results = response.json().get('results')
     if results:
         manufacturer = results[0]['device_type']['manufacturer']['name']
+        print(f"[INFO] Manufacturer: {manufacturer}")
         return manufacturer
     else:
         print("[ERROR] No manufacturer received from Netbox!")
         exit(1)
 
-def restart_remote_board(target, username, password):
+def restart_remote_board(target, username, password, force):
 
     # Set the authentication headers for Redfish
     auth = HTTPBasicAuth(username, password)
     headers = {'Content-Type': 'application/json'}
 
-    print(f"[INFO] Manufacturer: {manufacturer}")
+    # Dell iDRAC does not support ForceRestart Value
+    if force and manufacturer != "Dell":
+        reset_payload = {
+            "ResetType": "ForceRestart"
+        }
+    else:
+        reset_payload = {
+            "ResetType": "GracefulRestart"
+        }
 
-    if manufacturer == "HPE":
+    expected_status_codes = [200, 202, 204]
+
+    if manufacturer == "HPE" or manufacturer == "Lenovo":
         # Set the Redfish URL for resetting the iLO
-        redfish_url = f"https://{target}/redfish/v1/Managers/HpEthernetNetworkInterface/Reset"
-        reset_payload = {}
+        redfish_url = f"https://{target}/redfish/v1/Managers/1/Actions/Manager.Reset"
 
     elif manufacturer == "Dell":
 
         # Redfish URL to restart the iDRAC
         redfish_url = f'https://{target}/redfish/v1/Managers/iDRAC.Embedded.1/Actions/Manager.Reset'
         # JSON payload for the reset action
-        reset_payload = {
-            "ResetType": "GracefulRestart"
-        }
 
+    elif manufacturer == "Cisco":
+        redfish_url = f"https://{target}/redfish/v1/Managers/CIMC/Actions/Manager.Reset"
+
+    else:
+        print(f"[ERROR] Unknown manufacturer: {manufacturer}!")
+        exit(1)
+
+    print("[INFO] Sending restart request ...")
     try:
         response = requests.post(
             redfish_url,
             json = reset_payload,
             auth = auth, #(username, password),
             headers=headers,
+            timeout=args.timeout,
             verify=False  # Disabling SSL certificate verification for simplicity (not recommended in production)
         )
+        response.raise_for_status()
 
-    except Exception as e:
-        print(f"[ERROR] An error occurred contacting Remote Board: {str(e)}")
+    except requests.exceptions.Timeout:
+        print(f"[WARN] The restart request timed out, but the reset might still have been successful.")
+        exit()
+
+    except requests.exceptions.HTTPError as err:
+        print(f"[ERROR] An HTTP error occurred: {err}")
+        exit(1)
+
+    except requests.exceptions.RequestException as err:
+        print(f"[ERROR] An error occurred: {err}")
         exit(1)
 
     # Check if the request was successful
-    if response.status_code == 202 or response.status_code == 204:
-        print("[INFO] Remote Board restart initiated successfully")
+    if response and response.status_code in expected_status_codes:
+        print(f"[INFO] Remote Board restart initiated successfully: {response.status_code}")
     else:
         print(f"[ERROR] Failed to reset Remote Board: {response.status_code}")
         print(response.text)
@@ -101,9 +128,10 @@ if __name__ == '__main__':
     args = get_args()
 
     # Set the iLO IP address, username and password
-    target = args.ip_address
+    target = args.target
     username = args.username
     password = args.password
+    force = args.force
 
     print(f"[INFO] Restarting Remote Board with IP {target}")
 
@@ -111,17 +139,26 @@ if __name__ == '__main__':
         r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
     )
 
-    node_name_re = r"^(node\d{3})r-(\w{2}\d{3})\.cc.(\w{2}-\w{2}-\d)\.cloud\.sap$"
+    node_name_re = r"^(node\d{2,3})r-(\w{2}\d{3})\.cc.(\w{2}-\w{2}-\d)\.cloud\.sap$"
 
-    if ip_re.match(target):
+    if re.match(ip_re, target):
         try:
             host = socket.gethostbyaddr(target)[0]
         except socket.herror as err:
             print(f"[ERROR] Reverse DNS lookup failed for {target}")
-        if host:
+            exit(1)
+            
+    elif re.match(node_name_re, target):
+        host = target
+        
+    if host:
+        if re.match(node_name_re, host):
             matches = re.match(node_name_re, host).groups()
             netbox_device = matches[0] + "-" + matches[1]
             target = host
+        else:
+            print(f"[ERROR] Unknown hostname format {host}")
+            exit(1)
 
         print(f"[INFO] Resolved to hostname {host}")
     else:
@@ -129,4 +166,4 @@ if __name__ == '__main__':
         exit(1)
 
     manufacturer = get_manufacturer(netbox_device)  
-    restart_remote_board(target, username, password)
+    restart_remote_board(target, username, password, force)
