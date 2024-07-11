2808fe805e431582351bc0dc4bceda22c4dc87b3
d032408 <bernd.kuespert@sap.com>
2024-07-11 16:03:25 +0200

    add latests tag

--- dockerbuild.ps1
+++ dockerbuild.ps1
@@ -2,5 +2,5 @@ $version = get-date -Format yyyyMMddHHmmss
 $image = "inventory-updater"
 
 docker login keppel.eu-de-1.cloud.sap
-docker build . -t keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
+docker build . -t keppel.eu-de-1.cloud.sap/ccloud/${image}:$version -t keppel.eu-de-1.cloud.sap/ccloud/${image}:latest
 docker push keppel.eu-de-1.cloud.sap/ccloud/${image}:$version
